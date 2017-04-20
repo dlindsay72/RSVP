6434577a9defe7c2cbb90c66f985d7daa74e7760
Dan Lindsay <dlindsay72@hotmail.com>
2017-04-19 20:46:21 -0400

    Setup a submit event to add names, and setup RSVP checkbox styling when checked

---
+++ app.js
@@ -0,0 +1,30 @@
+const form = document.getElementById('registrar');
+const input = form.querySelector('input');
+const ul = document.getElementById('invitedList');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // prevents the page from reloading after an event has taken place
+    const text = input.value;
+    input.value = '';
+    const li = document.createElement('li');
+    li.textContent = text;
+    const label = document.createElement('label');
+    label.textContent = 'Confirmed';
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    label.appendChild(checkbox);
+    li.appendChild(label);
+    ul.appendChild(li);
+});
+
+ul.addEventListener('change', (event) => {
+    //console.log(event.target.checked);
+    const checkbox = event.target;
+    const checked = checkbox.checked;
+    const listItem = checkbox.parentNode.parentNode;
+    if (checked) {
+        listItem.className = 'responded';
+    } else {
+        listItem.className = '';
+    }
+});
