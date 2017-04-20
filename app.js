8c4c7b706b374ec42af85327842f203e66852e6e
Dan Lindsay <dlindsay72@hotmail.com>
2017-04-19 20:59:16 -0400

    refactored our submit listener and added createLi(text) method. Added listener to the remove button so we can now remove names when clicked

--- app.js
+++ app.js
@@ -2,18 +2,30 @@ const form = document.getElementById('registrar');
 const input = form.querySelector('input');
 const ul = document.getElementById('invitedList');
 
+function createLi(text) {
+    const li = document.createElement('li');
+    li.textContent = text;
+    const label = document.createElement('label');
+
+    label.textContent = 'Confirmed';
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+
+    label.appendChild(checkbox);
+    li.appendChild(label);
+
+    const button = document.createElement('button');
+    button.textContent = 'Remove';
+    li.appendChild(button);
+    return li;
+}
+
 form.addEventListener('submit', (event) => {
     event.preventDefault(); // prevents the page from reloading after an event has taken place
     const text = input.value;
     input.value = '';
-    const li = document.createElement('li');
-    li.textContent = text;
-    const label = document.createElement('label');
-    label.textContent = 'Confirmed';
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    label.appendChild(checkbox);
-    li.appendChild(label);
+    const li = createLi(text);
+
     ul.appendChild(li);
 });
 
@@ -26,5 +38,13 @@ ul.addEventListener('change', (event) => {
         listItem.className = 'responded';
     } else {
         listItem.className = '';
+    }
+});
+
+ul.addEventListener('click', (event) => {
+    if (event.target.tagName === 'BUTTON') {
+       const li = event.target.parentNode;
+       const ul = li.parentNode;
+       ul.removeChild(li); 
     }
 });
