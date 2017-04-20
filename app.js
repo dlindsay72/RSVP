adf96d2aa2f0c9b6fe60f99951c487f8a48251c0
Dan Lindsay <dlindsay72@hotmail.com>
2017-04-20 19:52:22 -0400

    Can now add, remove and edit names, as well as hide those who haven't responded with a  checkbox

--- app.js
+++ app.js
@@ -1,10 +1,45 @@
+document.addEventListener('DOMContentLoaded', () => { // wrapping DOMContentLoaded means we can add our javascript file anywhere in our html, and it will still load
 const form = document.getElementById('registrar');
 const input = form.querySelector('input');
+
+const mainDiv = document.querySelector('.main');
 const ul = document.getElementById('invitedList');
+
+const div = document.createElement('div');
+const filterLabel = document.createElement('label');
+const filterCheckbox = document.createElement('input');
+
+filterLabel.textContent = "Hide those who haven't responded";
+filterCheckbox.type = 'checkbox';
+div.appendChild(filterLabel);
+div.appendChild(filterCheckbox);
+mainDiv.insertBefore(div, ul);
+
+filterCheckbox.addEventListener('change', (e) => {
+    const isChecked = e.target.checked;
+    const lis = ul.children;
+    if(isChecked) {
+        for (let i = 0; i < lis.length; i += 1) {
+            let li = lis[i];
+            if (li.className === 'responded') {
+                li.style.display = '';
+            } else {
+                li.style.display = 'none';
+            }
+        }
+    } else {
+        for (let i = 0; i < lis.length; i += 1) {
+            let li = lis[i];
+            li.style.display = '';
+        }
+    }
+});
 
 function createLi(text) {
     const li = document.createElement('li');
-    li.textContent = text;
+    const span = document.createElement('span');
+    span.textContent = text;
+    li.appendChild(span);
     const label = document.createElement('label');
 
     label.textContent = 'Confirmed';
@@ -14,9 +49,13 @@ function createLi(text) {
     label.appendChild(checkbox);
     li.appendChild(label);
 
-    const button = document.createElement('button');
-    button.textContent = 'Remove';
-    li.appendChild(button);
+    const editButton = document.createElement('button');
+    editButton.textContent = 'edit';
+    li.appendChild(editButton);
+
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'remove';
+    li.appendChild(removeButton);
     return li;
 }
 
@@ -43,8 +82,27 @@ ul.addEventListener('change', (event) => {
 
 ul.addEventListener('click', (event) => {
     if (event.target.tagName === 'BUTTON') {
-       const li = event.target.parentNode;
-       const ul = li.parentNode;
-       ul.removeChild(li); 
+        const button = event.target;
+        const li = button.parentNode;
+        const ul = li.parentNode;
+        if (button.textContent === 'remove') {
+            ul.removeChild(li);
+        } else if (button.textContent === 'edit') {
+            const span = li.firstElementChild;
+            const input = document.createElement('input');
+            input.type = 'text';
+            input.value = span.textContent;
+            li.insertBefore(input, span);
+            li.removeChild(span);
+            button.textContent = 'save';
+        } else if (button.textContent === 'save') {
+            const input = li.firstElementChild;
+            const span = document.createElement('span');
+            span.textContent = input.value;
+            li.insertBefore(span, input);
+            li.removeChild(input);
+            button.textContent = 'edit';
+        }
     }
+});
 });
